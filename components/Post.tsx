b73bf2d44564e1bae487dd4454c53fdd05b91098
mohamed montaser <mastrocoding@gmail.com>
2025-11-12 01:28:37 +0200

    finish post statics

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero_Picture from "@/public/AC1_hero_desktop.avif";
 import dayjs from "@/lib/dayjs";
+import Love from "@/public/love-react.svg";
 
 export type TPost = {
   _id: string;
@@ -35,7 +36,7 @@ export default function Post({ post }: TProps) {
       : "grid-cols-2 grid-rows-2";
 
   return (
-    <>
+    <div className="post my-9 bg-post p-2.5 rounded-lg">
       <div className="header author-section">
         <section className="author flex gap-3 items-center">
           <Image
@@ -52,38 +53,54 @@ export default function Post({ post }: TProps) {
             </span>
           </div>
         </section>
-
-        <section className="post__content">
-          <p className="post__text text-slate-300 text-xl mb-4 mt-1">
-            {post.content}
-          </p>
-
-          <div className={`grid ${grid_cols_rows} gap-0.5 w-full h-96`}>
-            {post.images.length > 0 &&
-              post.images.map((image, i) => (
-                <div
-                  key={i}
-                  className={`relative ${
-                    i === 2 && post.images.length === 3 && "col-span-2"
-                  }`}
-                >
-                  <Image
-                    src={image}
-                    fill
-                    className={`object-cover`}
-                    alt={`post_image_${i}`}
-                  />
-
-                  {i === 3 && post.images.length > 4 && (
-                    <div className="w-full h-full bg-slate-400 opacity-80 text-black font-bold text-5xl flex justify-center items-center">
-                      +{post.images.length - 4}
-                    </div>
-                  )}
-                </div>
-              ))}
-          </div>
-        </section>
       </div>
-    </>
+      <section className="post__content">
+        <p className="post__text text-primary-text text-xl mb-4 mt-1">
+          {post.content}
+        </p>
+
+        <div className={`grid ${grid_cols_rows} gap-0.5 w-full h-96`}>
+          {post.images.length > 0 &&
+            post.images.map((image, i) => (
+              <div
+                key={i}
+                className={`relative ${
+                  i === 2 && post.images.length === 3 && "col-span-2"
+                }`}
+              >
+                <Image
+                  src={image}
+                  fill
+                  className={`object-cover`}
+                  alt={`post_image_${i}`}
+                />
+
+                {i === 3 && post.images.length > 4 && (
+                  <div className="w-full h-full bg-secondary opacity-80 text-black font-bold text-5xl flex justify-center items-center">
+                    +{post.images.length - 4}
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+      </section>
+      <section className="post_statics">
+        <div className="reacts flex justify-between items-center mt-2">
+          <div className="reacts__likes flex items-center gap-1">
+            <Image src={Love} alt="love-reacts" />
+            <span className="text-secondary">{post.statics.likes}</span>
+          </div>
+
+          <div className="reacts__comments-shares flex gap-4">
+            <span className="text-secondary text-sm">
+              {post.statics.shares} comments
+            </span>
+            <span className="text-secondary text-sm">
+              {post.statics.shares} shares
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
